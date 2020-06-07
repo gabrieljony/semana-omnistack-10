b7fb2d9b90840ff680ea7644153e7e12e5edb6b4
Gabriel Jony <gabjony@gmail.com>
2020-06-07 18:28:37 -0300

    Desenvolvendo o app mobile com Expo

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,14 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, YellowBox } from "react-native";
+import Routes from "./src/routes";
+
+YellowBox.ignoreWarnings(["Unrecognized WebSocket"]);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
+    <React.Fragment>
+      <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
+      <Routes />
+    </React.Fragment>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
