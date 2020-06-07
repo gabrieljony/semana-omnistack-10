eec989eee9faba30e56768ce06719d2742fc27ed
Gabriel Jony <gabjony@gmail.com>
2020-06-07 19:09:20 -0300

    Funcionalidades avançadas - Usando WebSocket

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const http = require("http");
 
 const app = express();
+const server = http.Server(app);
 
 mongoose.connect(
   "mongodb+srv://omnistack10:omnistack10@cluster0-huve3.mongodb.net/week10?retryWrites=true&w=majority",
