f4c0069ed5c263823e74bac07aaf3ef549e13475
Gabriel Jony <gabjony@gmail.com>
2020-06-07 17:08:19 -0300

    Correção: backend instalado a dependencia cors para o funcionamento do aplicação no localhost

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
@@ -12,6 +13,11 @@ mongoose.connect(
   }
 );
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use(routes);
 
